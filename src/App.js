7aa3c882376d6e83fe8b17266ab239ec44ee1d63
Zach Wilkins <zach.d.wilkins@gmail.com>
2024-03-15 14:45:19 -0400

    add login and register

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import { Page } from "./Components/Page/Page";
 
+import Register from "./user/Register";
+import Login from "./user/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/dashboard",
+    element: <Page />,
+  },
+]);
+
 function App() {
   return (
-    <div>
-      <Page />
-    </div>
+    <main className="App">
+      <RouterProvider router={router}></RouterProvider>
+      {/* <Page /> */}
+    </main>
   );
 }
 
